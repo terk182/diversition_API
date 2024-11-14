0037245cc184a2544024f64bb61abcbc6b66c12c
terk182 <ittipon.tana.terk@gmail.com>
2024-11-14 16:31:14 +0700

    add

--- App.Common/obj/Debug/net8.0/App.Common.AssemblyInfo.cs
+++ App.Common/obj/Debug/net8.0/App.Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df7e359c101ef4175b57c84903e9ecee3c2becba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6d06dfb756e17d4e4193c1e0d6e14642c1f4e6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("App.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App.Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
